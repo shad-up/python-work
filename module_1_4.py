a83e473478104f73c93acaa7d3552d72b63d4d40
shad-up <777123777z@gmail.com>
2024-11-13 02:59:14 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string =  input()
+print('количество символов введённого текста: ', my_string.count("")-1)#считает 'ввод' как доп. символ?
+print('количество символов введённого текста: ', len(my_string))
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.replace(' ', ''))
+print(my_string [0],my_string [-1])
