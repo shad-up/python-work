0c9a51de0049b9ff0419e2b64d87ee636be2da28
shad-up <777123777z@gmail.com>
2024-11-13 03:36:25 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 1.5, 'log', True
+print(immutable_var)
+# immutable_var [1] = 4 # кортеж содержит неизменяемые данные
+mutable_list = [1, 1.5, 'log', True]
+mutable_list [0] = 2
+mutable_list [1] = 2.2
+mutable_list [2] = int(13)
+print(mutable_list)
