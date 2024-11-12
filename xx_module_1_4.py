2422e68c397dd00b374567018fa7b3d7203a5dca
shad-up <777123777z@gmail.com>
2024-11-13 02:21:17 +0700

    Update and rename module_1_4.py to xx_module_1_4.py

---
+++ xx_module_1_4.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_title = 'Python'
+Time_for_one_task = Number_of_hours_spent/Number_of_homework_completed
+print('Курс: ', Course_title, ';', 'всего задач: ', Number_of_homework_completed, ';','затрачено часов:', Number_of_hours_spent, ';','среднее время выполнения:', Time_for_one_task)
